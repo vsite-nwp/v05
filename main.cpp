8bf870a51b967295c09d2c210c163b6bbc58be87
vanjs83 <tihomir.vanjurek@gmail.com>
2015-11-14 13:40:02 +0100

    create function OnPaint , typedef list and typedef const_iterator

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include <windows.h>
 #include <tchar.h>
 #include "nwpwin.h"
-
+#include<list>
+typedef std::list<POINT>list;
+typedef list::const_iterator iterator;
 class MainWindow : public Window
 {
+	list points;
 protected:
 	void OnPaint(HDC hdc)  
 	{ 
-	// TODO: iterate over points in container and draw polyline
+	// iterate over points in container and draw polyline
+		for (iterator iter = points.begin(); iter != points.end();++iter)
+		{
+			if (iter == points.begin())
+				MoveToEx(hdc, iter->x, iter->y, 0);
+			else
+				LineTo(hdc, iter->x, iter->y);
+		}
+			
 	}
 	void OnLButtonDown(POINT p) 
 	{
