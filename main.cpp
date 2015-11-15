2bb35f1031a203a90c0ab4272414ac663bf10b9b
zugcic <stzugcic@vsite.hr>
2015-11-15 19:29:37 +0100

    Napisan OnLButtonDown()&OnKeyDown()

--- main.cpp
+++ main.cpp
@@ -1,20 +1,36 @@
 #include <windows.h>
 #include <tchar.h>
 #include "nwpwin.h"
+#include<list>
 
 class MainWindow : public Window
 {
 protected:
+	std::list<POINT> lists;
+	std::list<POINT>::iterator it;
 	void OnPaint(HDC hdc)  
-	{ 
+	{
+	
 	// TODO: iterate over points in container and draw polyline
 	}
 	void OnLButtonDown(POINT p) 
 	{
+		lists.push_back(p);
 	// TODO: add point to container
 	}
 	void OnKeyDown(int vk) 
 	{
+		switch (vk) {
+		case VK_ESCAPE:
+			lists.clear();
+			break;
+		case VK_BACK:
+			lists.pop_back();
+			break;
+		default:
+			return;
+		}
+		
 	// TODO: Esc - empty container
 	// TODO: Backspace - remove last point
 	}
