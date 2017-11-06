de1805bd7fea2fdd62ddbfa40d8648450732df00
Luka N Sever <lukasever.56@gmail.com>
2017-11-06 20:46:07 +0100

    start

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include <windows.h>
 #include <tchar.h>
 #include "nwpwin.h"
+#include <list>
 
 class MainWindow : public Window
 {
+	std::list<POINT> points;
 protected:
 	void OnPaint(HDC hdc)  
 	{ 
 	// TODO: iterate over points in container and draw polyline
+		
 	}
 	void OnLButtonDown(POINT p) 
 	{
 	// TODO: add point to container
+		points.push_back(p);
 	}
 	void OnKeyDown(int vk) 
 	{
