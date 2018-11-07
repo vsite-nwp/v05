616e9b449015dd65b1e8008f6194843072e45990
Filip Kreč <filip.krec@yahoo.com>
2018-11-07 17:58:59 +0100

    Onpaint i Lbuttondown

--- main.cpp
+++ main.cpp
@@ -1,27 +1,43 @@
 #include <windows.h>
 #include <tchar.h>
 #include "nwpwin.h"
+#include <list>
 
 class MainWindow : public Window
 {
 protected:
 	void OnPaint(HDC hdc)  
 	{ 
-	// TODO: iterate over points in container and draw polyline
+		for (std::list<POINT>::const_iterator it = lista.begin(); it != lista.end(); ++it) {
+			if (it == lista.begin())
+				MoveToEx(hdc, it->x, it->y, NULL);
+			else 
+			LineTo(hdc, it->x, it->y);
+		}
+		InvalidateRect(*this, NULL, NULL);
 	}
 	void OnLButtonDown(POINT p) 
 	{
-	// TODO: add point to container
+		position = p;
+		lista.push_back(position);
+		UpdateWindow(*this);
 	}
 	void OnKeyDown(int vk) 
 	{
 	// TODO: Esc - empty container
+		switch (vk) {
+		case VK_ESCAPE: lista.clear(); break;
+		case VK_BACK: lista.pop_back(); break;
+		}
 	// TODO: Backspace - remove last point
 	}
 	void OnDestroy()
 	{
 		::PostQuitMessage(0);
 	}
+private:
+	std::list<POINT> lista;
+	POINT position;
 };
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
