bb3d3779c0e96758d1a417cd59553e5220bd0cc9
Ivan Batinić <ivek81cro@gmail.com>
2019-11-11 21:33:52 +0100

    vj5

--- main.cpp
+++ main.cpp
@@ -1,22 +1,61 @@
 #include <windows.h>
 #include <tchar.h>
 #include "nwpwin.h"
+#include <list>
+
+class SelObj {
+	HDC hdc;
+	HGDIOBJ hOld;
+public:
+	SelObj(HDC hdc, HGDIOBJ hObj) : hdc(hdc), hOld(::SelectObject(hdc, hObj)) { }
+	~SelObj() { ::SelectObject(hdc, hOld); }
+};
 
 class MainWindow : public Window
 {
+	std::list<POINT> points;
 protected:
 	void OnPaint(HDC hdc)  
 	{ 
-	// TODO: iterate over points in container and draw polyline
+		LineTo(hdc, 100, 100);
+		if (!points.empty()) 
+		{			
+			SelObj pen(hdc, GetStockObject(DC_PEN));
+			SetDCPenColor(hdc, RGB(255, 0, 0));
+			MoveToEx(hdc, points.front().x, points.front().y, 0);
+
+			for (POINT px : points)
+			{
+				LineTo(hdc, px.x, px.y);
+			}
+		}
+		MoveToEx(hdc, 100, 100, 0);
+		LineTo(hdc, 0, 200);
 	}
 	void OnLButtonDown(POINT p) 
 	{
-	// TODO: add point to container
+		points.push_back(p);
+		InvalidateRect(*this, NULL, true);
 	}
 	void OnKeyDown(int vk) 
 	{
-	// TODO: Esc - empty container
-	// TODO: Backspace - remove last point
+		switch (vk) {
+		case VK_BACK:
+			if (!points.empty()) {
+				points.pop_back();
+				InvalidateRect(*this, NULL, true);
+			}
+
+			break;
+		case VK_ESCAPE:
+			if (!points.empty()) {
+				points.clear();
+				InvalidateRect(*this, NULL, true);
+			}
+			break;
+		default:
+			InvalidateRect(*this, NULL, true);
+		}
 	}
 	void OnDestroy()
 	{
