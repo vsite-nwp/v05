bcc859f955c0bdabdb5a7dc5be8c27a077cdeace
Dona Pejnović <don@DESKTOP-QROSDFJ>
2019-11-12 11:42:17 +0100

    Iscrtavanje linija -program koristi hdc za iscrtavanje spojenih linija po zadanim točkama -točke se zadaju sa lijevim klikom miša -prošla točka se briše sa backspace tipkom -sve točke se brišu sa esc tipkom

--- main.cpp
+++ main.cpp
@@ -1,22 +1,38 @@
 #include <windows.h>
 #include <tchar.h>
 #include "nwpwin.h"
+#include <list>
 
 class MainWindow : public Window
 {
+private:
+		std::list<POINT> points;
 protected:
-	void OnPaint(HDC hdc)  
+	void OnPaint(HDC hdc)     //handler device complex
 	{ 
-	// TODO: iterate over points in container and draw polyline
+		if (points.empty()) return; 
+		MoveToEx(hdc, points.front().x, points.front().y, nullptr);
+		for (auto it : points)
+			::LineTo(hdc, it.x, it.y);
 	}
 	void OnLButtonDown(POINT p) 
 	{
-	// TODO: add point to container
+		points.push_back(p);
+		InvalidateRect(*this, nullptr, true);
 	}
 	void OnKeyDown(int vk) 
 	{
-	// TODO: Esc - empty container
-	// TODO: Backspace - remove last point
+		switch (vk) {
+		case VK_ESCAPE:
+			points.clear();
+			InvalidateRect(*this, nullptr, true);
+			break;
+		case VK_BACK:
+			if (!points.empty())
+				points.pop_back();
+			InvalidateRect(*this, nullptr, true);
+			break;
+		}
 	}
 	void OnDestroy()
 	{
