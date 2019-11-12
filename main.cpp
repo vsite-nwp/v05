be5c35aa2415ec5d80940d34888ec1bd217a4cba
Bruno Akrap <brakrap@vsite.hr>
2019-11-12 20:07:53 +0100

    NWP - vježba 5.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,44 @@
 #include <windows.h>
 #include <tchar.h>
+#include <list>
 #include "nwpwin.h"
 
 class MainWindow : public Window
 {
+	std::list<POINT> points;
+	
 protected:
 	void OnPaint(HDC hdc)  
 	{ 
-	// TODO: iterate over points in container and draw polyline
+		if (!points.empty())
+		{
+			MoveToEx(hdc, points.front().x, points.front().y, 0);
+
+			for (auto it = points.begin(); it != points.end(); ++it)
+			{
+				LineTo(hdc, (int)it->x, (int)it->y);
+			}
+		}
 	}
 	void OnLButtonDown(POINT p) 
 	{
-	// TODO: add point to container
+		points.push_back(p);
+		InvalidateRect(*this, NULL, true);
 	}
 	void OnKeyDown(int vk) 
 	{
-	// TODO: Esc - empty container
-	// TODO: Backspace - remove last point
+		switch (vk) {
+		case VK_ESCAPE:
+			points.clear();
+			InvalidateRect(*this, NULL, true);
+			break;
+		case VK_BACK:
+			points.pop_back();
+			InvalidateRect(*this, NULL, true);
+			break;
+		default:
+			return;
+		}
 	}
 	void OnDestroy()
 	{
