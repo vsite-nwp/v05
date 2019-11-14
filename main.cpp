cb0d976c1075b9165038dbd27a7f72abd2953f24
Marko Vitković <mavitkov@vsite.hr>
2019-11-15 00:30:20 +0100

    done all

--- main.cpp
+++ main.cpp
@@ -1,23 +1,61 @@
 #include <windows.h>
 #include <tchar.h>
 #include "nwpwin.h"
-
+#include <list>
+class Obj {
+	HDC handleDc;
+	HGDIOBJ hGetObj;
+public:
+	Obj(HDC hd, HGDIOBJ hGo) {
+		handleDc = hd;
+		hGetObj = hGo;
+	}
+	~Obj() {
+		SelectObject(handleDc, hGetObj);
+	}
+};
 class MainWindow : public Window
 {
+	std::list<POINT> pnt;
 protected:
-	void OnPaint(HDC hdc)  
-	{ 
-	// TODO: iterate over points in container and draw polyline
+	void OnPaint(HDC hdc)
+	{
+		if (!pnt.empty()) {
+
+			SetDCPenColor(hdc, RGB(255, 0, 0));
+			MoveToEx(hdc, pnt.front().x, pnt.front().y, 0);
+		}
+		for (auto it = pnt.begin(); it != pnt.end(); ++it)
+		{
+			LineTo(hdc, (int)it->x, (int)it->y);
+		}
 	}
 	void OnLButtonDown(POINT p) 
 	{
-	// TODO: add point to container
+		pnt.push_back(p);
+		InvalidateRect(*this, NULL, true);
 	}
 	void OnKeyDown(int vk) 
 	{
-	// TODO: Esc - empty container
-	// TODO: Backspace - remove last point
+		if (vk == VK_BACK)
+		{
+			if (!pnt.empty())
+				pnt.pop_back();
+		}
+
+		else if (vk == VK_ESCAPE)
+		{
+			if (!pnt.empty())
+				pnt.clear();
+		}
+		else
+		{
+			return;
+		}
+
+		InvalidateRect(*this, NULL, true);
 	}
+
 	void OnDestroy()
 	{
 		::PostQuitMessage(0);
