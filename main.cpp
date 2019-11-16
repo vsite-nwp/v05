40a7b859e3028ae78325ada14b71f2dc798d04e1
Senad Elezović <sen.elezovic@gmail.com>
2019-11-16 13:17:46 +0100

    Revert "riješen 5.labos"
    This reverts commit 8168ae375bfa00468e41b68aa0aac68f581f3bf2.

--- main.cpp
+++ main.cpp
@@ -1,48 +1,22 @@
 #include <windows.h>
 #include <tchar.h>
-#include <list>
 #include "nwpwin.h"
 
 class MainWindow : public Window
 {
 protected:
-	std::list<POINT> tocke;
-	std::list<POINT>::iterator it;
-	HPEN pen= CreatePen(PS_SOLID, 2, RGB(0,0,0));
 	void OnPaint(HDC hdc)  
-	{
-		POINT p;
-		SelectObject(hdc, pen);
-		if (tocke.begin() != tocke.end()) {
-			MoveToEx(hdc, (tocke.begin())->x, (tocke.begin())->y, &p);
-			for (it = ++tocke.begin(); it != tocke.end(); it++) {
-				LineTo(hdc, it->x, it->y);
-			}
-		}
-
-	
+	{ 
+	// TODO: iterate over points in container and draw polyline
 	}
 	void OnLButtonDown(POINT p) 
 	{
-		tocke.push_back(p);
-		InvalidateRect(*this, NULL, TRUE);
-		UpdateWindow(*this);
-	
+	// TODO: add point to container
 	}
 	void OnKeyDown(int vk) 
 	{
-		switch (vk) {
-		case VK_ESCAPE:
-			tocke.clear();
-			InvalidateRect(*this, NULL, TRUE);
-			UpdateWindow(*this);
-			break;
-		case VK_BACK:
-			tocke.pop_back();
-			InvalidateRect(*this, NULL, TRUE);
-			UpdateWindow(*this);
-			break;
-		}
+	// TODO: Esc - empty container
+	// TODO: Backspace - remove last point
 	}
 	void OnDestroy()
 	{
