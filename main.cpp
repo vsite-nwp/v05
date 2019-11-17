6b4f5050eedfb9aa0bf61a6dd6b7e2d4cbeb361d
Ivan <ivan.zoric98@gmail.com>
2019-11-17 17:46:43 +0100

    Solve vj5

--- main.cpp
+++ main.cpp
@@ -1,33 +1,52 @@
 #include <windows.h>
 #include <tchar.h>
 #include "nwpwin.h"
+#include <list>
 
 class MainWindow : public Window
 {
+	std::list<POINT> pt;
 protected:
-	void OnPaint(HDC hdc)  
-	{ 
-	// TODO: iterate over points in container and draw polyline
-	}
-	void OnLButtonDown(POINT p) 
+		void OnPaint(HDC hdc)
+		{
+			if (!pt.empty())
+			{
+				MoveToEx(hdc, pt.front().x, pt.front().y, nullptr);
+				for (auto it : pt)
+					LineTo(hdc, it.x, it.y);
+			}
+		}
+			void OnLButtonDown(POINT p)
+		{
+			pt.push_back(p);
+			InvalidateRect(*this, nullptr, true);
+		}
+			void OnKeyDown(int vk)
+		{
+			switch (vk)
+			{
+			case VK_ESCAPE:
+				pt.clear();
+				InvalidateRect(*this, nullptr, true);
+				break;
+			case VK_BACK:
+				if (!pt.empty())
+					pt.pop_back();
+				InvalidateRect(*this, nullptr, true);
+				break;
+			default:
+				break;
+			}
+		}
+		void OnDestroy()
+		{
+			::PostQuitMessage(0);
+		}
+	};
+	int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 	{
-	// TODO: add point to container
+		Application app;
+		MainWindow wnd;
+		wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, _T("NWP 5"));
+		return app.Run();
 	}
-	void OnKeyDown(int vk) 
-	{
-	// TODO: Esc - empty container
-	// TODO: Backspace - remove last point
-	}
-	void OnDestroy()
-	{
-		::PostQuitMessage(0);
-	}
-};
-
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
-{
-	Application app;
-	MainWindow wnd;
-	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, _T("NWP 5"));
-	return app.Run();
-}
