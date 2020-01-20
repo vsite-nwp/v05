0576a7297fe20a5a7725ed6075476ddbc3c52a4f
Marko Bohač <mabohac@vsite.hr>
2020-01-20 17:57:45 +0100

    Gotova vjezba

--- main.cpp
+++ main.cpp
@@ -1,22 +1,44 @@
 #include <windows.h>
 #include <tchar.h>
 #include "nwpwin.h"
+#include <list>
 
 class MainWindow : public Window
 {
+	std::list<POINT> plist;
 protected:
 	void OnPaint(HDC hdc)  
 	{ 
-	// TODO: iterate over points in container and draw polyline
+	// iterate over points in container and draw polyline
+		if (plist.empty()) return;
+		MoveToEx(hdc, plist.front().x, plist.front().y, nullptr);
+		for (POINT p : plist)
+			LineTo(hdc, p.x, p.y);
 	}
 	void OnLButtonDown(POINT p) 
 	{
-	// TODO: add point to container
+	// add point to container
+		plist.push_back(p);
+		InvalidateRect(*this, nullptr, true);
 	}
 	void OnKeyDown(int vk) 
 	{
-	// TODO: Esc - empty container
-	// TODO: Backspace - remove last point
+	// Esc - empty container
+	// Backspace - remove last point
+
+		switch (vk) {
+		case VK_ESCAPE:
+			plist.clear();
+			InvalidateRect(*this, nullptr, true);
+			break;
+		case VK_BACK:
+			if (plist.empty()) return;
+			plist.pop_back();
+			InvalidateRect(*this, nullptr, true);
+			break;
+		default:
+			return;
+		}
 	}
 	void OnDestroy()
 	{
