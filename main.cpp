248023dcefa4f11e5736311b732754d30e03b745
tirupcic <tirupcic@vsite.hr>
2021-04-04 15:19:49 +0200

    lab5 commit

--- main.cpp
+++ main.cpp
@@ -1,32 +1,55 @@
 #include <tchar.h>
 #include "nwpwin.h"
+#include <list>
 
 class main_window : public vsite::nwp::window
 {
+
 protected:
-	void on_paint(HDC hdc) override  
-	{ 
-	// TODO: iterate over points in container and draw polyline
+
+	void on_paint(HDC hdc) override{
+		if (!pontList.empty()) {
+			::MoveToEx(hdc, pontList.front().x, pontList.front().y, 0);
+		}
+
+		for (POINT points : pontList){
+			::LineTo(hdc, points.x, points.y);
+		}
 	}
-	void on_left_button_down(POINT p) override
-	{
-	// TODO: add point to container
+
+	void on_left_button_down(POINT points) override	{
+		pontList.push_back(points);
+		InvalidateRect(*this, 0, true);
 	}
-	void on_key_down(int vk) override
-	{
-	// TODO: Esc - empty container
-	// TODO: Backspace - remove last point
+
+	void on_key_down(int vk) override{
+		switch (vk){
+		case VK_ESCAPE:
+			pontList.clear();
+			::InvalidateRect(*this, nullptr, true);
+
+
+			break;
+		case VK_BACK:
+				pontList.pop_back();
+				::InvalidateRect(*this, nullptr, true);
+			break;
+		default:
+			return;
+		}
 	}
-	void on_destroy() override
-	{
+private:
+	std::list<POINT> pontList;
+
+
+	void on_destroy() override	{
 		::PostQuitMessage(0);
 	}
 };
 
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
-{
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int){
 	vsite::nwp::application app;
 	main_window w;
-	w.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 5");
+	w.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 5 lab");
 	return app.run();
 }
