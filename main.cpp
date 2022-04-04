ac64fff8d252b8ef1120e2afb6960cf895c3e710
Dovlek <vlado.tonkovic@outlook.com>
2022-04-04 13:08:40 +0200

    Resolve v05

--- main.cpp
+++ main.cpp
@@ -1,22 +1,49 @@
 #include <tchar.h>
 #include "nwpwin.h"
+#include <list>
 
 class main_window : public vsite::nwp::window
 {
 protected:
-	void on_paint(HDC hdc) override  
-	{ 
-	// TODO: iterate over points in container and draw polyline
+	std::list<POINT> draw_points;
+
+	void on_paint(HDC hdc) override
+	{
+		// iterate over points in container and draw polyline
+		if (!draw_points.empty())
+			MoveToEx(hdc, draw_points.front().x, draw_points.front().y, 0);
+		
+		for (auto it = draw_points.begin(); it != draw_points.end(); ++it)
+			LineTo(hdc, it->x, it->y);
 	}
+
 	void on_left_button_down(POINT p) override
 	{
-	// TODO: add point to container
+		// add point to container
+		draw_points.push_back(p);
+		InvalidateRect(*this, nullptr, true);
 	}
+
 	void on_key_down(int vk) override
 	{
-	// TODO: Esc - empty container
-	// TODO: Backspace - remove last point
+		// Esc - empty container
+		// Backspace - remove last point
+		if (vk == VK_BACK)
+		{
+			if (!draw_points.empty())
+				draw_points.pop_back();
+		}
+		else if (vk == VK_ESCAPE)
+		{
+			if (!draw_points.empty())
+				draw_points.clear();
+		}
+		else
+			return;
+
+		InvalidateRect(*this, nullptr, true);
 	}
+
 	void on_destroy() override
 	{
 		::PostQuitMessage(0);
