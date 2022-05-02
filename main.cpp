00ac35cc01e1083b4afa22d55a1f88f522668a64
Filip Lončar <filoncar@vsite.hr>
2022-05-02 19:36:28 +0200

    Works

--- main.cpp
+++ main.cpp
@@ -13,19 +13,37 @@ protected:
 	// TODO: iterate over points in container and draw polyline
 		for (auto point = list_p.begin(); point != list_p.end(); ++point) {
 
+			if (point == list_p.begin()) {
+				MoveToEx(hdc, point->x, point->y, NULL);
+			}
+			LineTo(hdc, point->x, point->y);
 		}
 	}
 	void on_left_button_down(POINT p) override
 	{
 	// TODO: add point to container
 		list_p.push_back(p);
+		//RedrawWindow(HDC,)
+		//UpdateWindow(GetDlgItem(hdc,GetActiveWindow());
+		//UpdateWindow(GetActiveWindow());
+		InvalidateRect(*this, nullptr, true);
+	
 	}
 	void on_key_down(int vk) override
 	{
 	// TODO: Esc - empty container
 		short esc_press = GetAsyncKeyState(VK_ESCAPE);
+		if (esc_press) {
+			list_p.clear();
+			InvalidateRect(*this, nullptr, true);
+		}
 	// TODO: Backspace - remove last point
-		short backs_press = GetAsyncKeyState(VK_BACKS);
+		short backspace_press = GetAsyncKeyState(VK_BACK);
+		if (backspace_press) {
+			if (!list_p.empty())
+				list_p.pop_back();
+			InvalidateRect(*this, nullptr, true);
+		}
 	}
 	void on_destroy() override
 	{
