f2f68505a6003f0528b1a497dc0a72d77fcfc23a
Ivor Novosel <ivnovosel@vsite.hr>
2023-04-03 20:47:30 +0200

    Complete polygon drawing

--- main.cpp
+++ main.cpp
@@ -1,26 +1,48 @@
 #include <tchar.h>
 #include "nwpwin.h"
+#include <list>
 
 class main_window : public vsite::nwp::window
 {
 protected:
 	void on_paint(HDC hdc) override  
 	{ 
-	// TODO: iterate over points in container and draw polyline
+	// iterate over points in container and draw polyline
+		if (points.empty()) return;
+		::MoveToEx(hdc, points.front().x, points.front().y, nullptr);
+		for (auto const& x : points) {
+			::LineTo(hdc, x.x, x.y);
+		}
 	}
 	void on_left_button_down(POINT p) override
 	{
-	// TODO: add point to container
+	// add point to container
+		points.push_back(p);
+		InvalidateRect(*this, NULL, false);
 	}
 	void on_key_down(int vk) override
 	{
-	// TODO: Esc - empty container
-	// TODO: Backspace - remove last point
+	// Esc - empty container
+	// Backspace - remove last point
+		if (points.empty()) return;
+		switch (vk) {
+		case VK_ESCAPE:
+			points.clear();
+			break;
+		case VK_BACK:
+			points.pop_back();
+			break;
+		default:
+			return;
+		}
+		InvalidateRect(*this, NULL, true);
 	}
 	void on_destroy() override
 	{
 		::PostQuitMessage(0);
 	}
+private:
+	std::list<POINT> points;
 };
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
