ff66a7d6ff6de5f90908e7109e7a5810069aca72
louis20th <lujomirs@gmail.com>
2023-04-03 20:55:21 +0200

    TODO's done

--- main.cpp
+++ main.cpp
@@ -1,21 +1,46 @@
 #include <tchar.h>
+#include <list>
+#include <algorithm>
 #include "nwpwin.h"
 
 class main_window : public vsite::nwp::window
 {
+private:
+	std::list<POINT> toDraw;
 protected:
 	void on_paint(HDC hdc) override  
 	{ 
 	// TODO: iterate over points in container and draw polyline
+		if (toDraw.empty()) return;
+		::MoveToEx(hdc, toDraw.front().x, toDraw.front().y, nullptr);
+		
+		std::for_each(toDraw.begin()++, toDraw.end(), [&hdc](const POINT& p) {
+			::LineTo(hdc, p.x, p.y);
+			});
 	}
 	void on_left_button_down(POINT p) override
 	{
-	// TODO: add point to container
+		toDraw.push_back(p);
+		::InvalidateRect(*this, nullptr, true);
 	}
 	void on_key_down(int vk) override
 	{
 	// TODO: Esc - empty container
 	// TODO: Backspace - remove last point
+		switch (vk)
+		{
+		case VK_ESCAPE:
+			toDraw.clear();
+			::InvalidateRect(*this, nullptr, true);
+			break;
+		case VK_BACK:
+			if(toDraw.empty())
+			 toDraw.pop_back();
+			::InvalidateRect(*this, nullptr, true);
+			break;
+		default:
+			break;
+		}
 	}
 	void on_destroy() override
 	{
