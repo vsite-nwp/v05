c1158b436cc378a32fcca9bfb7195182f6e8d2a9
Giancarlo <giancarlo.bedek@hotmail.com>
2023-06-03 13:45:00 +0200

    Remove polygon closing

--- main.cpp
+++ main.cpp
@@ -21,9 +21,6 @@ protected:
 		{
 			LineTo(hdc, points[i].x, points[i].y);
 		}
-
-		LineTo(hdc, points[0].x, points[0].y);
-
 	}
 	void on_left_button_down(POINT p) override
 	{
@@ -41,18 +38,18 @@ protected:
 		// Esc - empty container
 		case VK_ESCAPE:
 			points.clear();
+			InvalidateRect(*this, NULL, true);
 			break;
 
 		// Backspace - remove last point
 		case VK_BACK:
 			points.pop_back();
+			InvalidateRect(*this, NULL, true);
 			break;
 
 		default:
 			break;
 		}
-
-		InvalidateRect(*this, NULL, true);
 	}
 	void on_destroy() override
 	{
