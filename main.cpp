e31738fa2fc99f98bc61c4ae5f07d542773e6d67
Kristian Bolarić <krbolaric@vsite.hr>
2024-04-08 20:50:18 +0200

    Vjezba 5

--- main.cpp
+++ main.cpp
@@ -1,22 +1,36 @@
 #include <tchar.h>
 #include "nwpwin.h"
+#include <list>
 
 class main_window : public vsite::nwp::window
 {
+	std::list<POINT> points;
+
 protected:
-	void on_paint(HDC hdc) override  
-	{ 
-	// TODO: iterate over points in container and draw polyline
+	void on_paint(HDC hdc) override
+	{
+		for (auto point = points.begin(); point != points.end(); ++point) {
+			if (point == points.begin())
+				MoveToEx(hdc, point->x, point->y, NULL);
+			LineTo(hdc, point->x, point->y);
+		}
 	}
+
 	void on_left_button_down(POINT p) override
 	{
-	// TODO: add point to container
+		points.push_back(p);
+		InvalidateRect(*this, nullptr, true);
 	}
+
 	void on_key_down(int vk) override
 	{
-	// TODO: Esc - empty container
-	// TODO: Backspace - remove last point
+		if (vk == VK_ESCAPE)
+			points.clear();
+		if (vk == VK_BACK && !points.empty())
+			points.pop_back();
+		InvalidateRect(*this, nullptr, true);
 	}
+
 	void on_destroy() override
 	{
 		::PostQuitMessage(0);
