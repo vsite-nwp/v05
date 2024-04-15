63d66cd877c299487899be088e22cebb3cf73dfb
DrHrast <petar.huljek@gmail.com>
2024-04-15 20:03:26 +0200

    Napravio sam!!

--- main.cpp
+++ main.cpp
@@ -2,65 +2,83 @@
 #include "nwpwin.h"
 #include <vector>
 #include <algorithm>
+#include "gdi.h"
 
-class main_window : public vsite::nwp::window
-{
-private:
-	POINT point;
-	std::vector<POINT> points;
-	int dx = 0;
-	int dy = 0;
+namespace {
+	class sel_obj {
+		HDC hdc;
+		HGDIOBJ hOld;
+	public:
+		sel_obj(HDC hdc, HGDIOBJ hObj) : hdc(hdc), hOld(::SelectObject(hdc, hObj)) { }
+		~sel_obj() { ::SelectObject(hdc, hOld); }
+	};
 
-protected:
-	void on_paint(HDC hdc) override  
-	{ 
-	// TODO: iterate over points in container and draw polyline
-		if (!points.empty())
-			MoveToEx(hdc, points.front().x + dx, points.front().y + dy, nullptr);
-		for (auto p : points) {
-			LineTo(hdc, p.x + dx, p.y + dy);
+	class main_window : public vsite::nwp::window
+	{
+	private:
+		std::vector<POINT> points;
+		int dx = 0;
+		int dy = 0;
+
+	protected:
+		void on_paint(HDC hdc) override
+		{
+			using namespace vsite::nwp::gdi;
+			pen pencil(RGB(0, 0, 0), 5, PS_SOLID);
+			sel_obj sp(hdc, pencil);
+			brush brushie(RGB(0, 0, 0));
+			sel_obj sb(hdc, brushie);
+			// DID_IT: iterate over points in container and draw polyline
+			if (!points.empty()) {
+				MoveToEx(hdc, points.front().x + dx, points.front().y + dy, nullptr);
+				//::Ellipse(hdc, points.front().x - 5 + dx, points.front().y - 5 + dy, points.front().x + 5 + dx, points.front().y + 5 + dy);
+			}
+			for (auto p : points) {
+				LineTo(hdc, p.x + dx, p.y + dy);
+				//::Ellipse(hdc, p.x - 5 + dx, p.y - 5 + dy, p.x + 5 + dx, p.y + 5 + dy);
+			}
 		}
-	}
-	void on_left_button_down(POINT p) override
-	{
-	// TODO: add point to container
-		points.push_back(p);
-		::InvalidateRect(*this, nullptr, true);
-	}
-	void on_key_down(int vk) override
-	{
-	// TODO: Esc - empty container
-	// TODO: Backspace - remove last point
-		switch (vk) {
-		case VK_BACK:
-			if(!points.empty())
-				points.pop_back();
-			break;
-		case VK_ESCAPE:
-			points.clear();			
-			break;
-		case VK_UP:
-			dy--;
-			break;
-		case VK_DOWN:
-			dy++;
-			break;
-		case VK_LEFT:
-			dx--;
-			break;
-		case VK_RIGHT:
-			dx++;
-			break;
-		default:
-			return;
+		void on_left_button_down(POINT p) override
+		{
+			// DID_IT: add point to container
+			points.push_back(p);
+			::InvalidateRect(*this, nullptr, true);
 		}
-		::InvalidateRect(*this, nullptr, true);
-	}
-	void on_destroy() override
-	{
-		::PostQuitMessage(0);
-	}
-};
+		void on_key_down(int vk) override
+		{
+			// DID_IT: Esc - empty container
+			// DID_IT: Backspace - remove last point
+			switch (vk) {
+			case VK_BACK:
+				if (!points.empty())
+					points.pop_back();
+				break;
+			case VK_ESCAPE:
+				points.clear();
+				break;
+			case VK_UP:
+				dy--;
+				break;
+			case VK_DOWN:
+				dy++;
+				break;
+			case VK_LEFT:
+				dx--;
+				break;
+			case VK_RIGHT:
+				dx++;
+				break;
+			default:
+				return;
+			}
+			::InvalidateRect(*this, nullptr, true);
+		}
+		void on_destroy() override
+		{
+			::PostQuitMessage(0);
+		}
+	};
+}
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
