9d2b11e592bb8311c2ecf2fbd6a5dc632ead2212
Filip Les <ketvok@gmail.com>
2024-04-22 12:19:50 +0200

    Implement add point to container and draw polyline

--- main.cpp
+++ main.cpp
@@ -1,16 +1,35 @@
 #include <tchar.h>
 #include "nwpwin.h"
+#include <list>
 
 class main_window : public vsite::nwp::window
 {
+	std::list<POINT> points;
+
 protected:
 	void on_paint(HDC hdc) override  
 	{ 
 	// TODO: iterate over points in container and draw polyline
+		if (points.empty()) {
+			return;
+		}
+		
+		POINT firstPoint = points.front();
+		::MoveToEx(hdc, firstPoint.x, firstPoint.y, 0);
+
+		typedef std::list<POINT>::iterator iter;
+		iter it = points.begin();
+
+		for (++it; it != points.end(); ++it) {
+			POINT endPoint = *it;
+			::LineTo(hdc, endPoint.x, endPoint.y);
+		}
 	}
 	void on_left_button_down(POINT p) override
 	{
 	// TODO: add point to container
+		points.push_back(p);
+		::InvalidateRect(*this, 0, 0);
 	}
 	void on_key_down(int vk) override
 	{
@@ -27,6 +46,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
 	vsite::nwp::application app;
 	main_window w;
-	w.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 5");
+	w.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 5", 0, CW_USEDEFAULT, CW_USEDEFAULT, 400, 300);
 	return app.run();
 }
